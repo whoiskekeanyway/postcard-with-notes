3de26428f6a8fb9ff192ab2e9c1e68b4ef7f51fa
Seun O Ogundele <71834585+whoiskekeanyway@users.noreply.github.com>
2023-04-13 19:53:29 +0200

    fix-bugs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,6 +15,21 @@ footerYear.forEach((copyright) => {
   copyright.innerHTML = new Date().getFullYear();
 });
 
+function showOnHover(trigger, element) {
+  trigger.onmouseover = function () {
+    element.style.display = "block";
+    trigger.style.display = "none";
+  };
+  element.onmouseout = function () {
+    element.style.display = "none";
+    trigger.style.display = "block";
+  };
+}
+
+let trigger = document.getElementById("trigger");
+let element = document.getElementById("element-to-show");
+showOnHover(trigger, element);
+
 // Back to top button
 
 //Get the button
