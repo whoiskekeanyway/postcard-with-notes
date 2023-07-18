88f9f94f249a147ae889b82b7b92bfdb12f8fa08
Seun O Ogundele <71834585+whoiskekeanyway@users.noreply.github.com>
2023-07-18 12:00:47 +0200

    edited-portraits

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -7,15 +7,18 @@ var time = 3000; // Time Between Switch
 images[0] =
   "https://res.cloudinary.com/da93q413z/image/upload/v1669991144/postcardwithnotes/homepage/6_vtqbpn.webp";
 images[1] =
-  "https://res.cloudinary.com/da93q413z/image/upload/v1669991143/postcardwithnotes/homepage/5_kvoot9.webp";
+  "https://res.cloudinary.com/da93q413z/image/upload/v1689625085/postcardwithnotes/homepage/DSCF6331_x04l58.jpg";
 images[2] =
+  "https://res.cloudinary.com/da93q413z/image/upload/v1689621482/postcardwithnotes/homepage/_DSF4780-3_ef3da8.jpg";
+images[3] =
   "https://res.cloudinary.com/da93q413z/image/upload/v1669991143/postcardwithnotes/homepage/4_uwnymx.webp";
-//images[3] =
- // "https://res.cloudinary.com/da93q413z/image/upload/v1669991142/postcardwithnotes/homepage/3_zdiefm.webp";
-//images[4] =
-//  "https://res.cloudinary.com/da93q413z/image/upload/v1669991142/postcardwithnotes/homepage/2_j6f5cl.webp";
-//images[5] =
- // "https://res.cloudinary.com/da93q413z/image/upload/v1669991142/postcardwithnotes/homepage/1_wj5tlc.webp";
+
+images[4] =
+  "https://res.cloudinary.com/da93q413z/image/upload/v1689621482/postcardwithnotes/homepage/_DSF4779-2_sgr3ow.jpg";
+images[5] =
+  "https://res.cloudinary.com/da93q413z/image/upload/v1689624073/postcardwithnotes/homepage/_DSF4572_d7hob3.jpg";
+images[6] =
+  "https://res.cloudinary.com/da93q413z/image/upload/v1669991143/postcardwithnotes/homepage/5_kvoot9.webp";
 
 // Change Image
 function changeImg() {
